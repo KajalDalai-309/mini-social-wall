626bd793eceaa08d5b56e138da29319dc616d8a6
Kajal Dalai <kajal2005dalai@gmail.com>
2026-01-18 16:48:42 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -2,15 +2,19 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
-
 app.use("/uploads", express.static("uploads"));
+
+app.get("/", (req,res)=>{
+  res.send("Mini Social Wall Backend Running 🚀");
+});
 
 const postRoutes = require("./routes/postRoutes");
 app.use(postRoutes);
 
-const PORT = 5000;
-app.listen(PORT, () => {
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, ()=>{
   console.log("Server running on port " + PORT);
 });
